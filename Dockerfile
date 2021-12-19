dc5efdeac155a00a696d875e731c664660f946b1
Jash Parekh <jash389@gmail.com>
2021-12-19 18:16:15 -0500

    Initial Feature (#2)

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.10.1-slim AS builder
+ADD . /app
+WORKDIR /app
+
+# We are installing a dependency here directly into our app source dir
+RUN pip install --target=/app -r plugin_scripts/requirements.lock
+ENV PYTHONPATH /app
+
+CMD ["python", "/app/plugin_scripts/__init__.py"]
---
+++ docker/run_tests.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+BLACK_ACTION="--check"
+ISORT_ACTION="--check-only"
+
+function usage
+{
+    echo "usage: run_tests.sh [--format-code]"
+    echo ""
+    echo " --format-code : Format the code instead of checking formatting."
+    exit 1
+}
+
+while [[ $# -gt 0 ]]; do
+    arg="$1"
+    case $arg in
+        --format-code)
+        BLACK_ACTION="--quiet"
+        ISORT_ACTION="--profile black --atomic"
+        ;;
+        -h|--help)
+        usage
+        ;;
+        "")
+        # ignore
+        ;;
+        *)
+        echo "Unexpected argument: ${arg}"
+        usage
+        ;;
+    esac
+    shift
+done
+
+# only generate html locally
+pytest --cov plugin_scripts/ tests --cov-report html
+
+echo "Running MyPy..."
+mypy plugin_scripts tests
+
+echo "Running iSort..."
+isort ${ISORT_ACTION} plugin_scripts tests
+
+echo "Running black..."
+black ${BLACK_ACTION} plugin_scripts tests
+
+echo "Running flake8..."
+flake8 plugin_scripts tests
+
+echo "Running bandit..."
+bandit --ini .bandit --quiet -r plugin_scripts tests
